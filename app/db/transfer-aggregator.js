8e3de8102a55b1a8919b2a29c98fd81afcbd2d9b
Jose Bujalance <joseanbu23@gmail.com>
2018-08-18 20:38:44 +0200

    TransferAggregator

---
+++ app/db/transfer-aggregator.js
@@ -0,0 +1,59 @@
+const User = require('../model/user');
+
+class TransferAggregator {
+
+    constructor() {
+    }
+
+    getUsersBalance() {
+        return User.aggregate([
+            {
+                $match: {}
+            },{
+                $lookup: {
+                    from: "transfers",
+                    localField: "biwengerId",
+                    foreignField: "from",
+                    as: "gainTransfers"
+                }
+            },{
+                $lookup: {
+                    from: "transfers",
+                    localField: "biwengerId",
+                    foreignField: "to",
+                    as: "spendingTransfers"
+                }
+            },{
+                $project: {
+                    "_id": 0,
+                    "biwengerId": 1,
+                    "name": 1,
+                    "gain": {
+                        $reduce: {
+                            input: "$gainTransfers",
+                            initialValue: 0,
+                            in: { $add: ["$$value", "$$this.amount"] }
+                        }
+                    },
+                    "spend": {
+                        $reduce: {
+                            input: "$spendingTransfers",
+                            initialValue: 0,
+                            in: { $add: ["$$value", "$$this.amount"] }
+                        }
+                    }
+                }
+            },{
+                $addFields: {
+                    "balance": { $subtract: ["$gain", "$spend"] }
+                }
+            }
+        ], (err, res) => {
+            if (err) console.log('Error while calculating the gain aggregation: ' + err);
+            else return res;
+        });
+    }
+
+}
+
+module.exports = TransferAggregator;
---
+++ app/scripts/balance_aggregation.js
@@ -0,0 +1,43 @@
+db.getCollection('users').aggregate([
+    {
+        $match: {}
+    },{
+        $lookup: {
+            from: "transfers",
+            localField: "biwengerId",
+            foreignField: "from",
+            as: "gainTransfers"
+        }
+    },{
+        $lookup: {
+            from: "transfers",
+            localField: "biwengerId",
+            foreignField: "to",
+            as: "spendingTransfers"
+        }
+    },{
+        $project: {
+            "_id": 0,
+            "biwengerId": 1,
+            "name": 1,
+            "gain": {
+                $reduce: {
+                    input: "$gainTransfers",
+                    initialValue: 0,
+                    in: { $add: ["$$value", "$$this.amount"] }
+                }
+            },
+            "spend": {
+                $reduce: {
+                    input: "$spendingTransfers",
+                    initialValue: 0,
+                    in: { $add: ["$$value", "$$this.amount"] }
+                }
+            }
+        }
+    },{
+        $addFields: {
+            "balance": { $subtract: ["$gain", "$spend"] }
+        }
+    }
+])
