b29521fbc7ba6866865540e455ae0f94c84d2607
Jose Bujalance <joseanbu23@gmail.com>
2018-08-18 19:27:33 +0200

    Add aggregation scripts

---
+++ app/scripts/spending_aggregation.js
@@ -0,0 +1,20 @@
+db.getCollection('transfers').aggregate([
+    { 
+        $match: {}
+    },{ 
+        $group: {_id: "$to", minus: {$sum: "$amount"}}
+    },{ 
+        $lookup: {
+            from: "users",
+            localField: "_id",
+            foreignField: "biwengerId",
+            as: "user"
+        }
+    },{
+        $replaceRoot: {
+            newRoot: { $mergeObjects: [ { $arrayElemAt: [ "$user", 0 ] }, "$$ROOT" ] }
+        }
+    },{
+        $project: { user: 0, _id: 0, __v: 0}
+    }
+])
