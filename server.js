157a4e2ce7c859bf55ccaa2de8195b1601ac7f67
Jose Bujalance <joseanbu23@gmail.com>
2018-08-19 12:34:52 +0200

    Add root page endpoint

--- server.js
+++ server.js
@@ -10,8 +10,13 @@ mongoose.connect(process.env.DB_URI, { useNewUrlParser: true });
 // The aggregator that will provide the users balances
 var aggregator = new TransferAggregator();
 
+// Root page
+app.get('/', (req, res) => {
+    res.send('Up and runnig :)');
+});
+
 // Serve balances in dedicated endpoint
-app.get('/balances', (req, res) => {
+app.get('api/balances', (req, res) => {
     aggregator.getUsersBalance()
     .then(balances => {
         console.log('Serving request: ' + req.url);
