35aba95ea69ef9e6baf210e7341fe5f52deb62a3
Jose Bujalance <joseanbu23@gmail.com>
2018-08-28 20:46:42 +0200

    Update balance query

--- mongo_queries/balance_aggregation.js
+++ mongo_queries/balance_aggregation.js
@@ -1,43 +1,57 @@
-db.getCollection('users').aggregate([
-    {
-        $match: {}
+db.getCollection('users').aggregate([
+    {
+        $match: {}
+    },{
+        $lookup: {
+            from: "transfers",
+            localField: "biwengerId",
+            foreignField: "from",
+            as: "gainTransfers"
+        }
+    },{
+        $lookup: {
+            from: "transfers",
+            localField: "biwengerId",
+            foreignField: "to",
+            as: "spendingTransfers"
+        }
     },{
         $lookup: {
-            from: "transfers",
+            from: "roundstandings",
             localField: "biwengerId",
-            foreignField: "from",
-            as: "gainTransfers"
+            foreignField: "biwengerUserId",
+            as: "roundBonuses"
         }
-    },{
-        $lookup: {
-            from: "transfers",
-            localField: "biwengerId",
-            foreignField: "to",
-            as: "spendingTransfers"
-        }
-    },{
-        $project: {
-            "_id": 0,
-            "biwengerId": 1,
-            "name": 1,
-            "gain": {
-                $reduce: {
-                    input: "$gainTransfers",
-                    initialValue: 0,
-                    in: { $add: ["$$value", "$$this.amount"] }
-                }
+    },{
+        $project: {
+            "_id": 0,
+            "biwengerId": 1,
+            "name": 1,
+            "gain": {
+                $reduce: {
+                    input: "$gainTransfers",
+                    initialValue: 0,
+                    in: { $add: ["$$value", "$$this.amount"] }
+                }
+            },
+            "spend": {
+                $reduce: {
+                    input: "$spendingTransfers",
+                    initialValue: 0,
+                    in: { $subtract: ["$$value", "$$this.amount"] }
+                }
             },
-            "spend": {
+            "roundsBonus": {
                 $reduce: {
-                    input: "$spendingTransfers",
+                    input: "$roundBonuses",
                     initialValue: 0,
-                    in: { $add: ["$$value", "$$this.amount"] }
+                    in: { $add: ["$$value", "$$this.bonus"] }
                 }
-            }
-        }
-    },{
-        $addFields: {
-            "balance": { $subtract: ["$gain", "$spend"] }
-        }
-    }
+            }
+        }
+    },{
+        $addFields: {
+            "balance": { $add: ["$gain", "$spend", "$roundsBonus"] }
+        }
+    }
 ])
