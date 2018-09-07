3443a5e7a979ab3940693a35fe38a566dff5028f
Jose Bujalance <joseanbu23@gmail.com>
2018-09-07 20:34:04 +0200

    Remove env values

--- server.js
+++ server.js
@@ -1,12 +1,3 @@
-// PROD
-process.env.BIWENGER_BEARER = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOjI1NzA2OTE0LCJpYXQiOjE1MzQ0NDM2MTB9.9dmiHLTluMpoq8ZXorWZarsPa_r-i7hsGBg1I9IMdOA';
-process.env.BIWENGER_LEAGUE_ID = 504434;
-process.env.BIWENGER_X_VERSION = 553;
-process.env.DB_URI = 'mongodb+srv://biwenger:vg9kKsNFfcmkfNJa5Rm4@biwengercluster-pjbg7.gcp.mongodb.net/biwenger';
-process.env.PORT = 8080;
-process.env.JWT_SECRET = 'secret';
-
-
 const express = require('express');
 const app = express();
 const expressConfig = require('./app/config/express-config');
