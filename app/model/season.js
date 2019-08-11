a814243ecc79becdc8a2b7d4e2209aa26999347a
Jose Bujalance <joseanbu23@gmail.com>
2019-08-11 12:03:35 +0200

    Add getCurrentSeason to season model

--- app/model/season.js
+++ app/model/season.js
@@ -7,4 +7,11 @@ var season = new mongoose.Schema({
     name: String
 });
 
+/**
+ * @returns The current season, ie, the season that has the greatest key.
+ */
+season.statics.getCurrentSeason = function() {
+    return this.find().sort({'key': -1}).limit(1);
+}
+
 module.exports = mongoose.model('Season', season);
